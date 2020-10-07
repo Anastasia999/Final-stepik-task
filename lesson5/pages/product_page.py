dc784996380d842885c59294cd251a2f1977c08b
Anastasia999 <anastasiya.leonovich@mail.ru>
2020-10-07 19:05:36 +0700

    Added task 5.5 step2 about new test with the button Add product to cart

---
+++ lesson5/pages/product_page.py
@@ -0,0 +1,28 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+from selenium.common.exceptions import NoAlertPresentException
+import math
+
+class ProductPage(BasePage):
+    def add_to_cart(self):
+        button = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON)
+        button.click()
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
+
+    #def should_be_add_to_cart(self):
+        #assert ""
+
---
+++ lesson5/test_product_page.py
@@ -0,0 +1,11 @@
+from .pages.product_page import ProductPage
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.add_to_cart()
+    page.solve_quiz_and_get_code()
+
